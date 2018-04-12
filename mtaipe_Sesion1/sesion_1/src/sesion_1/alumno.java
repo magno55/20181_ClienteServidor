f447f89ecd404af209c0a317869b36aa9375b30c
magno55 <37269232+magno55@users.noreply.github.com>
2018-04-11 22:24:11 -0500

    Sesion 1

---
+++ mtaipe_Sesion1/sesion_1/src/sesion_1/alumno.java
@@ -0,0 +1,82 @@
+package sesion_1;
+
+public class alumno {
+    public alumno(String sNombre, int nEdad) {
+        super();
+        this.sNombre = sNombre;
+        this.nEdad = nEdad;        
+    }
+    String sNombre="Magno";
+    String sApellidos="Taipe Charca";
+    String sFechaNacimiento="15/05/1990";
+    String sCodigoAlumno="2014124369";
+    int sexo=1;
+
+    public void setSApellidos(String sApellidos) {
+        this.sApellidos = sApellidos;
+    }
+
+    public String getSApellidos() {
+        return sApellidos;
+    }
+
+    public void setSFechaNacimiento(String sFechaNacimiento) {
+        this.sFechaNacimiento = sFechaNacimiento;
+    }
+
+    public String getSFechaNacimiento() {
+        return sFechaNacimiento;
+    }
+
+    public void setSCodigoAlumno(String sCodigoAlumno) {
+        this.sCodigoAlumno = sCodigoAlumno;
+    }
+
+    public String getSCodigoAlumno() {
+        return sCodigoAlumno;
+    }
+
+    public void setSexo(int sexo) {
+        this.sexo = sexo;
+    }
+
+    public int getSexo() {
+        return sexo;
+    }
+
+    public void setSNombre(String sNombre) {
+        this.sNombre = sNombre;
+    }
+
+    public String getSNombre() {
+        return sNombre;
+    }
+
+    public void setNEdad(int nEdad) {
+        this.nEdad = nEdad;
+    }
+
+    public int getNEdad() {
+        return nEdad;
+    }
+    int nEdad;
+    public alumno() {
+        super();
+    }
+    public String SaludoBienvenida(String psNombre) {
+        return "Hola mundo!!. Bienvenid@ " + psNombre;
+    }
+    
+    public String Modificar()
+    {
+        
+        return "Se modificaron los datos";
+    }
+    
+    public static void main(String[] args) {
+        alumno myAlumno = new alumno();
+        for (int i = 0; i < 6; i++) {
+            System.out.println(i + myAlumno.SaludoBienvenida("Magno"));
+        }
+    }
+}
